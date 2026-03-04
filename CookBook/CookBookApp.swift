02790fe15bd68726d51da770c5b4be963b21ae97
Maryia Havarushka <maryia.havarushka@sunmait.tech>
2026-03-04 16:19:44 +0300

    initial

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Govorushko Mariya on 4.03.26.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
