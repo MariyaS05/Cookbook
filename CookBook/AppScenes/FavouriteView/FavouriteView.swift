6551630456b74b2ca55de3ac66016c0d9ce85a1b
MariyaS05 <mariya_govorushko@mail.ru>
2026-04-03 20:17:18 +0300

    favourite view

--- CookBook/AppScenes/FavouriteView/FavouriteView.swift
+++ CookBook/AppScenes/FavouriteView/FavouriteView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct FavouriteView: View {
+    @StateObject var viewModel = FavouriteViewModel()
     var body: some View {
-        Color.green
+        Group {
+            switch viewModel.viewsState {
+            case .loading:
+                ProgressView()
+                    .progressStyle()
+            case .loaded(let state):
+                recipesView(state.recipes)
+            }
+        }
+        .task {
+            viewModel.fetchStoredRecipes()
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background(.hexFDF6EC)
+    }
+    
+    private func recipesView(_ recipes: [Recipe]) -> some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: Layout.gridSpacing) {
+                ForEach(recipes, id: \.id) { recipe in
+                    Button {
+                        viewModel.presentDetailRecipeView(recipe.id)
+                    } label: {
+                        RecipeCardView(recipe: recipe)
+                    }
+                }
+            }
+        }
+
+        .contentMargins(.vertical, Layout.defaultSpacing)
+        .scrollIndicators(.hidden)
+        .padding(.horizontal, Layout.defaultSpacing)
     }
 }
 
 #Preview {
     FavouriteView()
+}
+
+extension FavouriteView {
+    private enum Layout {
+        static let gridSpacing: CGFloat = 12
+        static let categorySpacing: CGFloat = 13
+        static let headerSpacing: CGFloat = 10
+        static let defaultSpacing: CGFloat = 16
+    }
 }
--- CookBook/AppScenes/FavouriteView/FavouriteViewModel.swift
+++ CookBook/AppScenes/FavouriteView/FavouriteViewModel.swift
@@ -6,7 +6,53 @@
 //
 import SwiftUI
 import Combine
+import Factory
 
 final class FavouriteViewModel: ViewModel {
     
+    @Injected(\.recipeStoreService)
+    private var recipeStoreService
+    
+    @Injected(\.router)
+    private var router
+    
+    @Published
+    var viewsState: LoadingState = .loading
+    
+    override init() {
+        super.init()
+        
+        bind()
+    }
+    
+    @MainActor
+    func presentDetailRecipeView(_ id: String) {
+        router.push(.detailRecipe(id: id))
+    }
+    
+    func fetchStoredRecipes() {
+        recipeStoreService.fetchStoredRecipes()
+    }
+}
+
+extension FavouriteViewModel {
+    struct State {
+        var recipes: [Recipe] = []
+    }
+    
+    enum LoadingState {
+        case loading
+        case loaded(State)
+    }
+}
+
+private extension FavouriteViewModel {
+    func bind() {
+        recipeStoreService.recipeStoredData
+            .filter ({ !$0.isEmpty })
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] storedRecipes in
+                self?.viewsState = .loaded(.init(recipes: storedRecipes))
+            }.store(in: &cancellable)
+    }
 }
